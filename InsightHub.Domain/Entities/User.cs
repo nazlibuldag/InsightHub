4eb347b0804187dcaa3334dbcb5ae1c857d07118
nazlibuldag <nzlbldg@gmail.com>
2026-07-31 13:54:36 +0300

    Add project files.

---
+++ InsightHub.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InsightHub.Domain.Common;
+using InsightHub.Domain.Enums;
+
+namespace InsightHub.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public UserRole Role { get; set; } = UserRole.Analyst;
+}
---
+++ InsightHub.Domain/Enums/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsightHub.Domain.Enums;
+
+public enum UserRole
+{
+    Admin = 1,
+    Analyst = 2,
+    Manager = 3
+}
