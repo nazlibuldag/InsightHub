4808775b896632da856079fe9c6759cf6ed2b743
nazlibuldag <nzlbldg@gmail.com>
2026-08-03 11:16:44 +0300

    feat: implement dataset creation with Clean Architecture and CQRS

---
+++ InsightHub.API/Controllers/DatasetsController.cs
@@ -0,0 +1,29 @@
+﻿using InsightHub.Application.Features.Datasets.Commands.CreateDataset;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsightHub.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DatasetsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public DatasetsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateDatasetCommand command)
+    {
+        var datasetId = await _mediator.Send(command);
+
+        return Ok(new
+        {
+            Id = datasetId,
+            Message = "Dataset başarıyla oluşturuldu."
+        });
+    }
+}
